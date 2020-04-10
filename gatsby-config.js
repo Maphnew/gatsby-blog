d1ada1741afc8ccbeeb6ce645339c2d9ed1834eb
maphnew <zcm3118@gmail.com>
2020-04-10 17:22:45 +0900

    Change photo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Maphnew's Dev Blog`,
+    author: `Maphnew`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      github: `maphnew`,
     },
   },
   plugins: [
